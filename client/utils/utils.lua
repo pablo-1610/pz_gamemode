b0197053b14da398a21baa56aeda93e11dec29cb
Pablo <pablo.zapata.dev@gmail.com>
2021-03-10 19:03:59 +0100

    (system/utils): Changing client utils var

--- client/utils/utils.lua
+++ client/utils/utils.lua
@@ -1,11 +1,17 @@
-PZ.utils = {
-    requestModel = function(notHashedModel)
-        local model = GetHashKey(notHashedModel)
-        RequestModel(model)
-        while not HasModelLoaded(model) do Wait(1) end
-    end,
+PZ.clientUtils = {}
 
-    hash = function(notHashedModel)
-        return GetHashKey(notHashedModel)
+PZ.clientUtils.requestModel = function(notHashedModel)
+    local model = GetHashKey(notHashedModel)
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
     end
-}
+end
+
+PZ.clientUtils.toServer = function(str, ...)
+    TriggerServerEvent("pz:" .. GetHashKey(str), ...)
+end
+
+PZ.clientUtils.hash = function(notHashedModel)
+    return GetHashKey(notHashedModel)
+end
