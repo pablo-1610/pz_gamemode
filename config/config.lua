dab1f37c2a0edb489a69745a02001d0912790af7
Pablo <pablo.zapata.dev@gmail.com>
2021-03-10 18:42:48 +0100

    (system/config): Modifying config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,17 +1,21 @@
 PZ.config = {
-    base = {
+    devMode = true,
 
+    lang = "FR",
+
+
+    base = {
+        -- Default players spawns
+        defaultSpawns = {
+            {x = -9.96562, y = -1438.54, z = 31.1015, heading = 90.0}
+        }
     },
 
     jobs = {
 
     },
 
-    illegal = {
-
-    },
-
-    anticheat = {
+    antiCheat = {
 
     }
 }
