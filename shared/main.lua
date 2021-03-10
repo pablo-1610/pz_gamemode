b7f299adab307e2b6f829056cc82a3edf283cdc8
Pablo <pablo.zapata.dev@gmail.com>
2021-03-10 19:03:38 +0100

    (system/main): Adding PZ.sendInternal

--- shared/main.lua
+++ shared/main.lua
@@ -12,3 +12,7 @@ PZ.debug = function(str)
     if not PZ.config.devMode then return end
     print("^3[DEBUG]^7 "..str)
 end
+
+PZ.sendInternal = function(str, ...)
+    TriggerEvent("pz:"..GetHashKey(str), ...)
+end
