ef1df9e08759a8165a4a84d19d87e2f5e6500dfa
Pablo <pablo.zapata.dev@gmail.com>
2021-03-11 11:01:15 +0100

    (system/base): Update player.lua

--- server/players/player.lua
+++ server/players/player.lua
@@ -15,30 +15,40 @@ setmetatable(PZPlayer, {
     end
 })
 
----getDimension
----
+--- getId
+--- Get the current player's server id
+---@public
+---@return number
+function PZPlayer:getId()
+    return self.id
+end
+
+--- getName
+--- Get the current player's name
+---@public
+---@return string
+function PZPlayer:getName()
+    return self.name
+end
+
+--- getDimension
 --- Get current player's dimension
----
 ---@public
 ---@return number
 function PZPlayer:getDimension()
     return GetPlayerRoutingBucket(self.id)
 end
 
----setDimension
----
+--- setDimension
 --- Set the current player's dimension
----
 ---@public
 ---@return number
 function PZPlayer:setDimension(dimID)
     SetEntityRoutingBucket(self.id, dimID)
 end
 
----getLicense
----
+--- getLicense
 --- Get the current player's license
----
 ---@public
 ---@return string
 function PZPlayer:getLicense()
@@ -50,10 +60,8 @@ function PZPlayer:getLicense()
     return "err"
 end
 
----notify
----
+--- notify
 --- Notify the player
----
 ---@public
 ---@return void
 function PZPlayer:notify(str)
