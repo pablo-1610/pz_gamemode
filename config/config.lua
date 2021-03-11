a3f0d1c87eda9f3323b27b6ba9eff06afaa34452
Pablo <pablo.zapata.dev@gmail.com>
2021-03-11 12:36:03 +0100

    (system/richpresence): Implementing rich presence into config.lua

--- config/config.lua
+++ config/config.lua
@@ -3,6 +3,20 @@ PZConfig = {
 
     lang = "FR",
 
+    richPresence = {
+        enabled = true,
+        discord_appId = 703334106947125358,
+        discord_bigLogo = 'main',
+        discord_bigLogoText = 'This is a test',
+        discord_smallLogo = '',
+        discord_smallLogoText = '',
+        discord_defaultText = "A RolePlay-Based Game-mode !",
+        discord_buttons = {
+            -- Maximum 2 buttons allowed !
+            {title = "Discord", redirect = "https://discord.gg/m7X4ruA4rT"},
+            {title = "Website", redirect = "https://pablo-z.com"}
+        }
+    },
 
     base = {
         -- Default players spawns
