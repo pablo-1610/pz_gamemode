35b454ada64f9c11acf4ae777a45e642f03c4ba7
Pablo <pablo.zapata.dev@gmail.com>
2021-03-11 11:01:00 +0100

    (system/base): Update PZPlayersManager

--- server/players/manager.lua
+++ server/players/manager.lua
@@ -1,25 +1,46 @@
-PZ.players = {}
+---@class PZPlayersManager
+PZPlayersManager = {}
 
-PZ.playersManager = {
-    ---@return void
-    create = function(playerID)
-        local player = PZPlayer(playerID)
-        PZ.players[playerID] = player
-        PZ.debug("Creating a player ("..player.id.."), name: "..player.name)
-    end,
+---players
+---@public
+PZPlayersManager.players = {}
 
-    ---@return void
-    remove = function(playerID)
-        PZ.players[playerID] = nil
-    end,
+---create
+---@public
+---@return void
+PZPlayersManager.create = function(playerID)
+    local player = PZPlayer(playerID)
+    PZ.players[playerID] = player
+    PZ.debug("Creating a player (" .. player.id .. "), name: " .. player.name)
+end
 
-    ---@return table
-    getPlayers = function()
-        return PZ.players
-    end,
+---remove
+---@public
+---@return void
+PZPlayersManager.remove = function(playerID)
+    PZPlayersManager.players[playerID] = nil
+end
 
-    ---@return PZPlayer
-    getPlayer = function(playerID)
-        return PZ.players[playerID] or {}
+---getPlayer
+---@public
+---@return table
+PZPlayersManager.getPlayers = function()
+    return PZPlayersManager.players
+end
+
+---getPlayer
+---@public
+---@return PZPlayer
+PZPlayersManager.getPlayer = function(playerID)
+    return PZPlayersManager.players[playerID]
+end
+
+---notifyAll
+---@public
+---@return void
+PZPlayersManager.notifyAll = function(message)
+    ---@param player PZPlayer
+    for k, player in pairs(PZPlayersManager.players) do
+        player:notify(message)
     end
-}
+end
