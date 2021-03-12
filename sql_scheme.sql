6e2440d1374549302fef7faffa96f422bcfc63cf
Pablo <pablo.zapata.dev@gmail.com>
2021-03-13 00:33:04 +0100

    (github/base): Update the SQL scheme

--- sql_scheme.sql
+++ sql_scheme.sql
@@ -1,2 +1,6 @@
 -- Db init
 CREATE DATABASE pz_gamemode;
+-- Table users
+CREATE TABLE `pz`.`pz_users` ( `id` INT NOT NULL AUTO_INCREMENT , `license` VARCHAR(80) NOT NULL , `name` TEXT NOT NULL , `rank` INT NOT NULL , `rolePlayIdentity` TEXT NOT NULL , `createdAt` TEXT NOT NULL , `updatedAt` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+-- Table ranks
+CREATE TABLE `pz`.`pz_ranks` ( `id` INT NOT NULL AUTO_INCREMENT , `identifier` TEXT NOT NULL , `display` TEXT NOT NULL , `color` TEXT NOT NULL , `permissions` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
