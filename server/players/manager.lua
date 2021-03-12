1e8e0ae16e4a69596f1be86dd5d072ea38b833f0
Pablo <pablo.zapata.dev@gmail.com>
2021-03-12 23:38:23 +0100

    (system/server): Modifying players account management

--- server/players/manager.lua
+++ server/players/manager.lua
@@ -9,9 +9,26 @@ PZPlayersManager.players = {}
 ---@public
 ---@return void
 PZPlayersManager.create = function(playerID)
-    local player = PZPlayer(playerID)
-    PZPlayersManager.players[playerID] = player
-    PZShared.debug("Creating a player (" .. player.id .. "), name: " .. player.name)
+    local player
+    PZDb.AsyncFetchAll("SELECT * FROM pz_users WHERE license = @a", {['@a'] = PZServer.getPlayerLicense(playerID)}, function(result)
+        if result[1] then
+            player = PZPlayer(playerID, PZServer.getPlayerLicense(playerID), result[1].name, json.decode(result[1].rolePlayIdentity))
+        else
+            PZShared.debug("Player isn't in the D.B")
+            PZDb.AsyncExecute("INSERT INTO pz_users (license, name, rolePlayIdentity, createdAt, updatedAt) VALUES(@a,@b,@c,@d,@e)", {
+                ['a'] = PZServer.getPlayerLicense(playerID),
+                ['b'] = GetPlayerName(playerID),
+                ['c'] = json.encode({}),
+                ['d'] = "", -- Add timestamp
+                ['e'] = "" -- Add timestamp
+            })
+        end
+        if not player then
+            player = PZPlayer(playerID, PZServer.getPlayerLicense(playerID), GetPlayerName(playerID), {})
+        end
+        PZPlayersManager.players[playerID] = player
+        PZShared.debug("Creating a player (" .. player.id .. "), name: " .. player.name)
+    end)
 end
 
 ---remove
